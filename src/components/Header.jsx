614f230b23a288c569280c1de587dab394ad11f5
Manas0410 <manasshrivastava0410@gmail.com>
2025-02-12 23:06:26 +0530

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,67 @@
-import { useState } from "react";
 import { CompanyName, navData } from "../constants";
+import React, { useState, useEffect, useRef } from "react";
 
 const Header = () => {
   const [MenuOpen, setMenuOpen] = useState(false);
+  const navRef = useRef(null); // Ref for the navigation menu
+
+  const Navigation = () => (
+    <nav
+      ref={navRef} // Attach the ref to the navigation menu
+      className="flex flex-col gap-4 w-[200px] p-8 rounded-lg shadow-md text-white justify-center items-center hover:cursor-pointer bg-orange-light absolute top-[144%] right-0"
+    >
+      {navData.map((item, i) => {
+        return (
+          <a
+            onClick={() => setMenuOpen(false)}
+            key={i}
+            href={item.path}
+            className="hover:bg-white hover:text-orange text-center w-full text-xl"
+          >
+            {item.label}
+          </a>
+        );
+      })}
+    </nav>
+  );
+
+  // Effect to handle clicks outside the navigation menu
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    };
+
+    // Add event listener when the menu is open
+    if (MenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    // Cleanup the event listener
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [MenuOpen]);
 
   return (
     <>
-      <header className=" flex sm:hidden  items-center h-[52px] justify-between px-4 py-2 bg-orange text-white font-edo sticky top-0 z-50">
+      <header className="flex sm:hidden items-center h-[52px] justify-between px-4 py-2 bg-orange text-white font-edo sticky top-0 z-50">
         <h1 className="text-3xl font-bold">{CompanyName}</h1>
-        <label className="burger" htmlFor="burger">
-          <input
-            type="checkbox"
-            id="burger"
-            checked={MenuOpen}
-            onChange={() => setMenuOpen(!MenuOpen)}
-          />
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
+        <div className="relative">
+          <label className="burger" htmlFor="burger">
+            <input
+              type="checkbox"
+              id="burger"
+              checked={MenuOpen}
+              onChange={() => setMenuOpen(!MenuOpen)}
+            />
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+          {MenuOpen && <Navigation />}
+        </div>
       </header>
       {/* <div className="h-[52px]" /> */}
     </>
